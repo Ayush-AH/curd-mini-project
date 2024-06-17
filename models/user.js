8f53e00187621a8fd730cd72b0ec4182bb44d2a1
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-06-17 14:17:31 +0530

    message

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://ayushahirwar6673:1rfP4qhZAKE78NP3@cluster0.3mtk3nu.mongodb.net/CURDproject?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://ayushtest:ayushtest@cluster0.y6lpkpw.mongodb.net/curdprojectt?retryWrites=true&w=majority&appName=Cluster0")
 .then(function(){
     console.log("connected to mongodb");
 })
